4a507ef64656bed30ad4126436c046db755d8239
Arun <arunbpatil603@gmail.com>
2024-08-23 14:19:11 +0530

    Changes for compilation after merge
    - Changes for compilation after merge

--- app/src/main/java/com/analogics/tpaymentsapos/rootUiScreens/dashboard/view/DashboardScreenView.kt
+++ app/src/main/java/com/analogics/tpaymentsapos/rootUiScreens/dashboard/view/DashboardScreenView.kt
@@ -5,13 +5,6 @@ import android.content.Context
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.DrawerValue
 import androidx.compose.material.ModalDrawer
@@ -20,6 +13,8 @@ import androidx.compose.material.Surface
 import androidx.compose.material.rememberDrawerState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
