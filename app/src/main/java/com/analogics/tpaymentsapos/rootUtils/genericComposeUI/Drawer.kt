66995befd3d55fef41a79687b833132e494921c0
Arun <arunbpatil603@gmail.com>
2024-09-04 10:51:10 +0530

    Resolved merge conflicts
    - Resolved merge conflicts

--- app/src/main/java/com/analogics/tpaymentsapos/rootUtils/genericComposeUI/Drawer.kt
+++ app/src/main/java/com/analogics/tpaymentsapos/rootUtils/genericComposeUI/Drawer.kt
@@ -1,6 +1,7 @@
 package com.analogics.tpaymentsapos.rootUtils.genericComposeUI
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
