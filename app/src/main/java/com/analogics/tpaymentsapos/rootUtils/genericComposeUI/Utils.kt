94846f022cacc30ceb9904813607a26dc671e216
Anjali <anjali.kapoor@eazypaytech.com>
2024-09-25 23:59:35 +0530

    add getall record from db query and logic

--- app/src/main/java/com/analogics/tpaymentsapos/rootUtils/genericComposeUI/Utils.kt
+++ app/src/main/java/com/analogics/tpaymentsapos/rootUtils/genericComposeUI/Utils.kt
@@ -43,10 +43,9 @@ fun formatAmount(input: String, decimalPlaces: Int = 2, symbol: Symbol?=Symbol()
         val dAmount: Double = amount.toDoubleOrNull()?:0.00
         val currency = symbol?.get()?:""
         val separator : String = if(withSeparator) "," else ""
-        val spaceChar : String = if(symbol?.noSpace==true) "" else " "
         return when(symbol?.position) {
-            Symbol.Position.START -> "$currency$spaceChar%${separator}.${decimalPlaces}f".format(Locale.ENGLISH, dAmount / 10.0.pow(decimalPlaces))
-            Symbol.Position.END -> "%.${decimalPlaces}f$spaceChar$currency".format(Locale.ENGLISH, dAmount / 10.0.pow(decimalPlaces))
+            Symbol.Position.START -> "$currency %${separator}.${decimalPlaces}f".format(Locale.ENGLISH, dAmount / 10.0.pow(decimalPlaces))
+            Symbol.Position.END -> "%.${decimalPlaces}f $currency".format(Locale.ENGLISH, dAmount / 10.0.pow(decimalPlaces))
             else -> "%.${decimalPlaces}f".format(Locale.ENGLISH, dAmount / 10.0.pow(decimalPlaces))
         }
     } catch (e: Exception) {
@@ -54,6 +53,7 @@ fun formatAmount(input: String, decimalPlaces: Int = 2, symbol: Symbol?=Symbol()
     }
     return ""
 }
+
 
 fun calculateTip(amount: Double, tip: Double): Double {
     return amount * tip
