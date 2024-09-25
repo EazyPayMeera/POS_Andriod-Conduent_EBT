a32e4e42c9f6f55010f527d8351774b22ea57f09
Anjali <anjali.kapoor@eazypaytech.com>
2024-09-25 21:46:58 +0530

    add getall record from db query and logic

--- app/src/main/java/com/analogics/tpaymentsapos/rootUiScreens/txnList/viewModel/TxnViewModel.kt
+++ app/src/main/java/com/analogics/tpaymentsapos/rootUiScreens/txnList/viewModel/TxnViewModel.kt
@@ -1,29 +1,48 @@
 package com.analogics.tpaymentsapos.rootUiScreens.txnList.viewModel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.analogics.securityframework.database.dbRepository.TxnDBRepository
+import com.analogics.securityframework.database.entity.TxnEntity
+import com.analogics.tpaymentsapos.rootModel.ObjRootAppPaymentDetails
 import com.analogics.tpaymentsapos.rootUiScreens.txnList.model.TxnDataList
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-open class TxnViewModel  : ViewModel() {
-    val _transactionList = MutableStateFlow<List<TxnDataList>>(emptyList())
+@HiltViewModel
+class TxnViewModel @Inject constructor(private val dbRepository: TxnDBRepository) : ViewModel() {
+    private val _transactionList = MutableStateFlow<List<TxnDataList>>(emptyList())
     val transactionList: StateFlow<List<TxnDataList>> = _transactionList
+    private var allTransactionList: List<TxnEntity>? = null
 
     init {
-        // Simulate data fetching
+        // Fetch transactions asynchronously
         viewModelScope.launch {
-            _transactionList.value = fetchTransactions()
+            fetchTransactions()
         }
     }
 
-    private fun fetchTransactions(): List<TxnDataList> {
-        // This would normally come from a repository or database
-        return listOf(
-            TxnDataList(1, "Today @ 14:15:30", "Purchase", 450.00, true),
-            TxnDataList(2, "Today @ 14:15:30", "Refund", 50.00, false),
-            TxnDataList(3, "26-2-2020 @ 14:15:30", "Purchase", 50.00, true)
-        )
+    fun fetchTransactions() {
+        viewModelScope.launch {
+            allTransactionList = dbRepository.getAllTxnListData()
+            Log.d("db data", allTransactionList.toString())
+            allTransactionList?.let {
+                val txnDataList = convertTxnEntityListToTxnDataList(it)
+                _transactionList.value = txnDataList
+            }
+        }
+    }
+
+    private fun convertTxnEntityListToTxnDataList(txnEntityList: List<TxnEntity>): List<TxnDataList> {
+        val gson = Gson()
+        val json = gson.toJson(txnEntityList)
+        val txnDataListType = object : TypeToken<List<TxnDataList>>() {}.type
+        return gson.fromJson(json, txnDataListType)
     }
 }
