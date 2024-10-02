2d4bc43cd2e8c3840c870758e52f4846d0a40d5c
Rahul Karande <Rahul.X.Karande@aib.ie>
2024-10-02 11:21:27 +0530

    Update TPaymentsApplication.kt
    code merged

--- app/src/main/java/com/analogics/tpaymentsapos/application/TPaymentsApplication.kt
+++ app/src/main/java/com/analogics/tpaymentsapos/application/TPaymentsApplication.kt
@@ -1,9 +1,8 @@
 package com.analogics.tpaymentsapos.application
 
 import android.app.Application
-import com.analogics.paymentservicecore.repository.paymentService.PaymentServiceRepository
+
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 
 @HiltAndroidApp
 class TPaymentsApplication:Application() {
