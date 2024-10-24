2e37f62bd6abc031f29e71e21b996394f1f720db
rushikesh-eazypay <rushikesh@eazypaytech.com>
2024-10-24 12:37:30 +0530

    Set Language Hindi as per Selection
    Set Language Hindi as per Selection

--- app/src/main/java/com/analogics/tpaymentsapos/rootUiScreens/language/view/LanguageView.kt
+++ app/src/main/java/com/analogics/tpaymentsapos/rootUiScreens/language/view/LanguageView.kt
@@ -18,11 +18,11 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.analogics.tpaymentsapos.R
@@ -36,10 +36,10 @@ import com.analogics.tpaymentsapos.rootUtils.genericComposeUI.TextView
 import com.analogics.tpaymentsapos.ui.theme.dashboardOrangeColor
 import com.analogics.tpaymentsapos.ui.theme.dimens
 
-
 @Composable
 fun LanguageView(navHostController: NavHostController, viewModel: LanguageViewModel = hiltViewModel()) {
-    var sharedViewModel = localSharedViewModel.current
+    val context = LocalContext.current // Get the context here
+    val sharedViewModel = localSharedViewModel.current
 
     Column {
         CommonTopAppBar(
@@ -56,85 +56,32 @@ fun LanguageView(navHostController: NavHostController, viewModel: LanguageViewMo
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier.padding(
                     top = MaterialTheme.dimens.DP_30_CompactMedium,
-                    bottom = MaterialTheme.dimens.DP_11_CompactMedium)
+                    bottom = MaterialTheme.dimens.DP_11_CompactMedium
+                )
             ) {
-
                 TextView(
                     text = stringResource(id = R.string.select_lang),
                     fontSize = MaterialTheme.dimens.SP_21_CompactMedium,
                     color = MaterialTheme.colorScheme.tertiary,
                     fontWeight = FontWeight.Bold,
-                    1,
-                    Modifier.padding(MaterialTheme.dimens.DP_24_CompactMedium),
+                    modifier = Modifier.padding(MaterialTheme.dimens.DP_24_CompactMedium),
                     textAlign = TextAlign.Center
                 )
 
                 ImageView(
-                    imageId = R.drawable.language, size = MaterialTheme.dimens.DP_33_CompactMedium,
-                    shape = RectangleShape, // Example shape, can be any Shape
+                    imageId = R.drawable.language,
+                    size = MaterialTheme.dimens.DP_33_CompactMedium,
+                    shape = RectangleShape,
                     alignment = Alignment.Center,
-                    contentDescription = "",
+                    contentDescription = ""
                 )
 
                 Spacer(modifier = Modifier.height(MaterialTheme.dimens.DP_11_CompactMedium))
 
-                // Divider after "English"
                 Divider(
                     color = MaterialTheme.colorScheme.tertiary,
                     thickness = MaterialTheme.dimens.DP_1_CompactMedium,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 0.dp)
-                )
-
-                Spacer(modifier = Modifier.height(MaterialTheme.dimens.DP_11_CompactMedium))
-
-                // Row for "Hindi" with RadioButton
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = MaterialTheme.dimens.DP_24_CompactMedium)
-                ) {
-
-                    Icon(
-                        painter = painterResource(id = R.drawable.orange_bullet), // Replace with your orange bullet drawable resource
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.primary, // Adjust the color to match the bullet color
-                        modifier = Modifier
-                            .size(MaterialTheme.dimens.DP_25_CompactMedium) // Adjust size as needed
-                            .padding(end = MaterialTheme.dimens.DP_10_CompactMedium) // Spacing between bullet and text
-                    )
-
-                    TextView(
-                        text = stringResource(id = R.string.english),
-                        fontSize = MaterialTheme.dimens.SP_21_CompactMedium,
-                        color = MaterialTheme.colorScheme.tertiary,
-                        //fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(MaterialTheme.dimens.DP_15_CompactMedium),
-                        textAlign = TextAlign.Center
-                    )
-
-                    Spacer(modifier = Modifier.weight(1f)) // This will push the RadioButton to the end
-
-                    RadioButton(
-                        selected = viewModel.uiLanguage.value == UiLanguage.ENGLISH,
-                        onClick = { viewModel.onLanguageChange(UiLanguage.ENGLISH, sharedViewModel) },
-                        colors = RadioButtonDefaults.colors(
-                            selectedColor = MaterialTheme.colorScheme.primary,
-                            unselectedColor = MaterialTheme.colorScheme.onSecondary
-                        )
-                    )
-                }
-
-                Spacer(modifier = Modifier.height(MaterialTheme.dimens.DP_11_CompactMedium))
-                // Divider after "Hindi"
-                Divider(
-                    color = MaterialTheme.colorScheme.tertiary,
-                    thickness = MaterialTheme.dimens.DP_1_CompactMedium,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = MaterialTheme.dimens.DP_0_CompactMedium)
+                    modifier = Modifier.fillMaxWidth()
                 )
 
                 Spacer(modifier = Modifier.height(MaterialTheme.dimens.DP_11_CompactMedium))
@@ -146,39 +93,82 @@ fun LanguageView(navHostController: NavHostController, viewModel: LanguageViewMo
                         .fillMaxWidth()
                         .padding(horizontal = MaterialTheme.dimens.DP_24_CompactMedium)
                 ) {
-
                     Icon(
-                        painter = painterResource(id = R.drawable.orange_bullet), // Replace with your orange bullet drawable resource
+                        painter = painterResource(id = R.drawable.orange_bullet),
                         contentDescription = null,
-                        tint = MaterialTheme.colorScheme.primary, // Adjust the color to match the bullet color
+                        tint = MaterialTheme.colorScheme.primary,
                         modifier = Modifier
-                            .size(MaterialTheme.dimens.DP_25_CompactMedium) // Adjust size as needed
-                            .padding(end = MaterialTheme.dimens.DP_10_CompactMedium) // Spacing between bullet and text
+                            .size(MaterialTheme.dimens.DP_25_CompactMedium)
+                            .padding(end = MaterialTheme.dimens.DP_10_CompactMedium)
                     )
+
+                    TextView(
+                        text = stringResource(id = R.string.english),
+                        fontSize = MaterialTheme.dimens.SP_21_CompactMedium,
+                        color = MaterialTheme.colorScheme.tertiary,
+                        modifier = Modifier.padding(MaterialTheme.dimens.DP_15_CompactMedium),
+                        textAlign = TextAlign.Center
+                    )
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    RadioButton(
+                        selected = viewModel.uiLanguage.value == UiLanguage.ENGLISH,
+                        onClick = { viewModel.onLanguageChange(UiLanguage.ENGLISH, sharedViewModel, context) },
+                        colors = RadioButtonDefaults.colors(
+                            selectedColor = MaterialTheme.colorScheme.primary,
+                            unselectedColor = MaterialTheme.colorScheme.onSecondary
+                        )
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(MaterialTheme.dimens.DP_11_CompactMedium))
+
+                Divider(
+                    color = MaterialTheme.colorScheme.tertiary,
+                    thickness = MaterialTheme.dimens.DP_1_CompactMedium,
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.height(MaterialTheme.dimens.DP_11_CompactMedium))
+
+                // Row for "Hindi" with RadioButton
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = MaterialTheme.dimens.DP_24_CompactMedium)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.orange_bullet),
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier
+                            .size(MaterialTheme.dimens.DP_25_CompactMedium)
+                            .padding(end = MaterialTheme.dimens.DP_10_CompactMedium)
+                    )
+
                     TextView(
                         text = stringResource(id = R.string.hindi),
                         fontSize = MaterialTheme.dimens.SP_21_CompactMedium,
                         color = MaterialTheme.colorScheme.tertiary,
-                        //fontWeight = FontWeight.Bold,
                         modifier = Modifier.padding(MaterialTheme.dimens.DP_15_CompactMedium),
                         textAlign = TextAlign.Center
                     )
-                    Spacer(modifier = Modifier.weight(1f)) // This will push the RadioButton to the end
+
+                    Spacer(modifier = Modifier.weight(1f))
 
                     RadioButton(
                         selected = viewModel.uiLanguage.value == UiLanguage.HINDI,
-                        onClick = { viewModel.onLanguageChange(UiLanguage.HINDI, sharedViewModel)},
+                        onClick = { viewModel.onLanguageChange(UiLanguage.HINDI, sharedViewModel, context) },
                         colors = RadioButtonDefaults.colors(
                             selectedColor = dashboardOrangeColor,
                             unselectedColor = MaterialTheme.colorScheme.onSecondary
                         )
                     )
                 }
-
             }
-
         }
-
     }
 
     LaunchedEffect(Unit) {
