89c510dd04ec7c878afa1a5dca973f3dbfd0c79b
rushikesh-eazypay <rushikesh@eazypaytech.com>
2024-11-15 12:52:17 +0530

    Configuration View Adjustment Done
    Configuration View Adjustment Done

--- app/src/main/java/com/analogics/tpaymentsapos/rootUiScreens/settings/config/Configuration.kt
+++ app/src/main/java/com/analogics/tpaymentsapos/rootUiScreens/settings/config/Configuration.kt
@@ -1,6 +1,5 @@
 package com.analogics.tpaymentsapos.rootUiScreens.settings.config
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -38,8 +37,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import com.analogics.tpaymentsapos.rootUiScreens.settings.config.ConfigViewModel
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -142,7 +139,7 @@ fun ConfigurationView(navHostController: NavHostController, viewModel: ConfigVie
         Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(MaterialTheme.dimens.DP_24_CompactMedium),
+                .padding(MaterialTheme.dimens.DP_10_CompactMedium),
             elevation = CardDefaults.elevatedCardElevation(MaterialTheme.dimens.DP_11_CompactMedium)
         ) {
             LazyColumn {
@@ -226,7 +223,7 @@ fun TippingView(
         modifier = Modifier
             .fillMaxWidth()
             .background(color = MaterialTheme.colorScheme.onPrimary)
-            .padding(MaterialTheme.dimens.DP_20_CompactMedium)
+            .padding(MaterialTheme.dimens.DP_4_CompactMedium)
     ) {
         Column(
             modifier = Modifier.fillMaxWidth()
@@ -236,8 +233,8 @@ fun TippingView(
                 style = MaterialTheme.typography.bodyMedium,
                 textAlign = TextAlign.Center,
                 modifier = Modifier.fillMaxWidth()
+                    .padding(bottom = MaterialTheme.dimens.DP_22_CompactMedium)
             )
-
             if (type == ConfigurableViewType.Inactivity_Timeout) {
                 Box(
                     modifier = Modifier
@@ -278,7 +275,8 @@ fun TippingView(
                 }
 
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(MaterialTheme.dimens.DP_24_CompactMedium),
                     horizontalArrangement = Arrangement.SpaceEvenly
                 ) {
                     options.forEach { option ->
