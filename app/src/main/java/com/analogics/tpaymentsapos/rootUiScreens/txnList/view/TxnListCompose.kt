cb6a654211ec85d7c8b748a93271070135dcf95f
rushikesh-eazypay <rushikesh@eazypaytech.com>
2024-11-15 17:34:50 +0530

    Printer Circular Menu Align on DashBoard , Txn List and Txn Detail Screen
    1.Printer Circular Menu Align on DashBoard , Txn List and Txn Detail Screen
    2. Printer Image in Circular Menu is in Center of Circle

--- app/src/main/java/com/analogics/tpaymentsapos/rootUiScreens/txnList/view/TxnListCompose.kt
+++ app/src/main/java/com/analogics/tpaymentsapos/rootUiScreens/txnList/view/TxnListCompose.kt
@@ -765,9 +765,9 @@ fun CircularMenu(
                 }
         ) {
             Image(
-                painter = painterResource(id = R.drawable.printer_logo), // Replace with your image resource
+                painter = painterResource(id = R.drawable.print_logo), // Replace with your image resource
                 contentDescription = stringResource(id = R.string.print), // Provide a content description for accessibility
-                modifier = Modifier.size(androidx.compose.material3.MaterialTheme.dimens.DP_60_CompactMedium) // Adjust size as needed
+                modifier = Modifier.size(androidx.compose.material3.MaterialTheme.dimens.DP_60_CompactMedium)
             )
         }
     }
