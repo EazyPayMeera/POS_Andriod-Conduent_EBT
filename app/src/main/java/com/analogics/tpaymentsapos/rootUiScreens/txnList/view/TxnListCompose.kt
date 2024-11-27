f55f1eec76a5bd0412771455d0702a1da19c4d59
rushikesh-eazypay <rushikesh@eazypaytech.com>
2024-11-27 10:40:12 +0530

    Progress Indicator Updated
    Progress Indicator Updated

--- app/src/main/java/com/analogics/tpaymentsapos/rootUiScreens/txnList/view/TxnListCompose.kt
+++ app/src/main/java/com/analogics/tpaymentsapos/rootUiScreens/txnList/view/TxnListCompose.kt
@@ -24,12 +24,12 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -47,6 +47,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -198,12 +199,13 @@ fun TransactionListScreen(
                         Box(
                             modifier = Modifier
                                 .fillMaxSize()
-                                .padding(24.dp),
-                            contentAlignment = Alignment.Center
+                                .padding(androidx.compose.material3.MaterialTheme.dimens.DP_4_CompactMedium),
+                            contentAlignment = Alignment.TopCenter
                         ) {
-                            LinearProgressIndicator(
+                            CircularProgressIndicator(
                                 color = androidx.compose.material3.MaterialTheme.colorScheme.primary,
-                                //trackColor = Color.LightGray
+                                modifier = Modifier.size(androidx.compose.material3.MaterialTheme.dimens.DP_100_CompactMedium)
+                                    .graphicsLayer(alpha = 0.4f)
                             )
                         }
                     }
