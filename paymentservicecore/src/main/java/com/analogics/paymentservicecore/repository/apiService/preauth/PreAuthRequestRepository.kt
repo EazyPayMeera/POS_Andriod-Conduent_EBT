ae2b33817fe20aa286767f1ca65d852e912c7f1c
rushikesh-eazypay <rushikesh@eazypaytech.com>
2024-12-10 12:29:20 +0530

    Reapply "nj"
    This reverts commit 1b8251155c3e1bbd5bbd328fc883688547fab8df.

---
+++ paymentservicecore/src/main/java/com/analogics/paymentservicecore/repository/apiService/preauth/PreAuthRequestRepository.kt
@@ -0,0 +1,115 @@
+package com.analogics.paymentservicecore.repository.apiService.preauth
+
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import com.analogics.builder_core.constants.BuilderConstants
+import com.analogics.builder_core.listener.responseListener.IBuilderServiceResponseListener
+import com.analogics.builder_core.listener.responseListener.IBuilderServiceResponseListenerLyra
+import com.analogics.builder_core.model.BuilderServiceTxnDetails
+import com.analogics.builder_core.repository.BuilderServiceRepository
+import com.analogics.builder_core.repository.BuilderServiceRepositoryLyra
+import com.analogics.builder_core.requestBuilder.ApiRequestBuilder
+import com.analogics.builder_core.requestBuilder.ApiRequestBuilderLyra
+import com.analogics.builder_core.utils.BuilderUtils
+import com.analogics.paymentservicecore.constants.AppConstants
+import com.analogics.paymentservicecore.constants.EmvConstants
+import com.analogics.paymentservicecore.model.PaymentServiceTxnDetails
+import com.analogics.paymentservicecore.model.error.ApiServiceError
+import com.analogics.paymentservicecore.models.TxnStatus
+import com.analogics.paymentservicecore.utils.PaymentServiceUtils
+import com.analogics.tpaymentcore.utils.TlvUtils
+import javax.inject.Inject
+
+class PreAuthRequestRepository @Inject constructor(
+    var apiRequestBuilder: ApiRequestBuilder,
+    private var builderServiceRepository: BuilderServiceRepository,
+    var apiRequestBuilderLyra: ApiRequestBuilderLyra,
+    private var builderServiceRepositoryLyra: BuilderServiceRepositoryLyra
+) {
+
+    //lateinit var paymentServiceTxnDetails:PaymentServiceTxnDetails
+    @RequiresApi(Build.VERSION_CODES.O)
+    suspend fun sendPreAuthRequest(paymentServiceTxnDetails: PaymentServiceTxnDetails?, onAPIServiceResponse:(Any)->Unit) {
+
+        if(paymentServiceTxnDetails?.acquirerName == AppConstants.ACQUIRER_LYRA) {
+            var request = apiRequestBuilderLyra.createPurchaseRequest(
+                PaymentServiceUtils.transformObject<BuilderServiceTxnDetails>(
+                    paymentServiceTxnDetails
+                )
+            )
+
+            if(paymentServiceTxnDetails.isDemoMode == true)
+            {
+                onAPIServiceResponse(parseIsoRespMessage(paymentServiceTxnDetails,
+                    apiRequestBuilderLyra.buildDummyPurchaseResponse()))
+            }
+            else {
+                builderServiceRepositoryLyra.networkServiceRequest(
+                    object :
+                        IBuilderServiceResponseListenerLyra {
+                        override fun onBuilderSuccess(response: ByteArray) {
+                            onAPIServiceResponse(parseIsoRespMessage(paymentServiceTxnDetails,response))
+                        }
+
+                        override fun onBuilderFailure(error: Any) {
+                            onAPIServiceResponse(ApiServiceError(error.toString()))
+                        }
+                    },
+                    request
+                )
+            }
+        }
+        else {
+            builderServiceRepository.apiPurchase(
+                object : IBuilderServiceResponseListener {
+                    override fun onBuilderSuccess(response: String) {
+                        onAPIServiceResponse(response)
+                        Log.d("record insert", "onApiSuccessRes")
+
+                    }
+
+                    override fun onBuilderFailure(error: Any) {
+                        Log.d("record insert", "onApiFailureRes")
+                        onAPIServiceResponse(ApiServiceError(error.toString()))
+                        paymentServiceTxnDetails?.let { onAPIServiceResponse(it) }
+                    }
+                },
+                BuilderUtils.prepareApiRequestBody(
+                    apiRequestBuilder.createPurchaseRequest(
+                        PaymentServiceUtils.transformObject<BuilderServiceTxnDetails>(
+                            paymentServiceTxnDetails
+                        )
+                    )
+                )
+
+            )
+        }
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    fun parseIsoRespMessage(paymentServiceTxnDetails : PaymentServiceTxnDetails, response: ByteArray) : PaymentServiceTxnDetails {
+        apiRequestBuilderLyra.parsePurchaseResponse(response).let {
+            paymentServiceTxnDetails.hostRespCode = it.hostRespCode
+            paymentServiceTxnDetails.hostAuthCode = it.hostAuthCode
+            paymentServiceTxnDetails.hostTxnRef = it.hostTxnRef
+            var tlv = TlvUtils(it.emvData)
+            /* Extract tag 8A from ISO field if required */
+            if(tlv.tlvMap.containsKey(EmvConstants.EMV_TAG_RESP_CODE)==false) {
+                it.hostRespCode?.encodeToByteArray()?.toHexString()?.let {
+                    tlv.tlvMap[EmvConstants.EMV_TAG_RESP_CODE] = it
+                }
+            }
+            paymentServiceTxnDetails.emvData = tlv.toTlvString()
+            if (it.hostRespCode == BuilderConstants.ISO_RESP_CODE_APPROVED) {
+                paymentServiceTxnDetails.hostAuthResult = TxnStatus.APPROVED.toString()
+                paymentServiceTxnDetails.txnStatus = TxnStatus.APPROVED.toString()
+            }
+            else {
+                paymentServiceTxnDetails.hostAuthResult = TxnStatus.DECLINED.toString()
+                paymentServiceTxnDetails.txnStatus = TxnStatus.DECLINED.toString()
+            }
+        }
+        return paymentServiceTxnDetails
+    }
+}
