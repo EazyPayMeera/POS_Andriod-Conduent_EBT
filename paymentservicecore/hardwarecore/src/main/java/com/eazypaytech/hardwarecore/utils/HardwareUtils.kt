330ef740f26b1a96ba0f4f79c19d63ef1c03aec6
Arun <arunbpatil603@gmail.com>
2025-04-15 16:13:18 +0530

    Cancelled last changes
    - Cancelled last changes

--- paymentservicecore/hardwarecore/src/main/java/com/eazypaytech/hardwarecore/utils/HardwareUtils.kt
+++ paymentservicecore/hardwarecore/src/main/java/com/eazypaytech/hardwarecore/utils/HardwareUtils.kt
@@ -4,6 +4,9 @@ import android.device.DeviceManager
 import android.device.SEManager
 import android.util.Log
 import com.eazypaytech.tpaymentcore.constants.EncryptionConstants
+import com.eazypaytech.tpaymentcore.repository.EmvSdkRequestRepository
+import com.eazypaytech.tpaymentcore.repository.EmvWrapperRepository
+import com.urovo.sdk.pinpad.PinPadProviderImpl
 
 object HardwareUtils {
     fun getDeviceSN() : String
@@ -15,8 +18,7 @@ object HardwareUtils {
     fun injectTMK(tmk: String, kcv: String) : Boolean
     {
         try {
-            return true
-            //return PinPadProviderImpl.getInstance().loadMainKey(EncryptionConstants.KEY_INDEX_MAIN_KEY, tmk.hexToByteArray(), kcv.hexToByteArray())
+            return PinPadProviderImpl.getInstance().loadMainKey(EncryptionConstants.KEY_INDEX_MAIN_KEY, tmk.hexToByteArray(), kcv.hexToByteArray())
         }catch (exception : Exception)
         {
             Log.e("HARDWARE_UTILS", exception.message.toString())
@@ -30,10 +32,9 @@ object HardwareUtils {
         try {
             var ipekBytes = ipek.hexToByteArray()
             var ksnBytes = ksn.hexToByteArray()
-            return true
-//            return PinPadProviderImpl.getInstance().downloadKeyDukpt(EncryptionConstants.DUKPT_KEY_SET_PIN, null,0, ksnBytes, ksnBytes.size, ipekBytes, ipekBytes.size) == 0 &&
-//                    PinPadProviderImpl.getInstance().downloadKeyDukpt(EncryptionConstants.DUKPT_KEY_SET_TDK, null,0, ksnBytes, ksnBytes.size, ipekBytes, ipekBytes.size) == 0 &&
-//                    PinPadProviderImpl.getInstance().downloadKeyDukpt(EncryptionConstants.DUKPT_KEY_SET_EMV, null,0, ksnBytes, ksnBytes.size, ipekBytes, ipekBytes.size) == 0
+            return PinPadProviderImpl.getInstance().downloadKeyDukpt(EncryptionConstants.DUKPT_KEY_SET_PIN, null,0, ksnBytes, ksnBytes.size, ipekBytes, ipekBytes.size) == 0 &&
+                    PinPadProviderImpl.getInstance().downloadKeyDukpt(EncryptionConstants.DUKPT_KEY_SET_TDK, null,0, ksnBytes, ksnBytes.size, ipekBytes, ipekBytes.size) == 0 &&
+                    PinPadProviderImpl.getInstance().downloadKeyDukpt(EncryptionConstants.DUKPT_KEY_SET_EMV, null,0, ksnBytes, ksnBytes.size, ipekBytes, ipekBytes.size) == 0
         }catch (exception : Exception)
         {
             Log.e("HARDWARE_UTILS", exception.message.toString())
